324244cfa59b96eb3d43a3ef17d29cf54dabe8db
Micah Mohr <contact@coopyey.gay>
2025-06-23 19:40:51 -0500

    finished new challenges for bookbot

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    char_count = get_char_counts(text)    
-    print_report(char_count, num_words)
+from stats import get_num_words
+import sys
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+def main():    
+    print("Input path to book: ", sys.argv)
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else: 
+        text = get_book_text(sys.argv[1])
+        num_words = get_num_words(text)
+        char_count = get_char_counts(text)    
+        print_report(char_count, num_words)
+
 def get_char_counts(text):
     lowered = text.lower()
     done = {}
@@ -28,16 +28,21 @@ def get_char_counts(text):
 
     return done
 
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
 def print_report(char_count, word_total):
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_total} found in the document\n")
+    print("--- Begin report of: ", sys.argv[1], "---")
+    print(f"{word_total} words found in the document\n")
 
     sorted_chars = {}
     sorted_chars = dict(sorted(char_count.items(), key=lambda item: item[1], reverse=True))
 
     for i in sorted_chars:
         if i.isalpha():
-            print(f"the character {i} was found {sorted_chars[i]} times in the document")
+            print(f"{i}: {sorted_chars[i]}")
 
     print("\n--- End of Report ---")
 
